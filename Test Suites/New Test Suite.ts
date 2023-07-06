18343df8467ef92218424dcaf55e896448edad11
Krishna Hegde <91774814+Krishna-D-Hegde@users.noreply.github.com>
2023-07-06 15:29:21 +0530

    first commit

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6005f501-8e7f-4084-a126-0bfe38ebae8d</testSuiteGuid>
+   <testCaseLink>
+      <guid>28dd1ae3-5963-417d-a4ee-6946b6a5a64e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
